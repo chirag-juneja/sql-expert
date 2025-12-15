28e2e70a72563336456bc05d77570315a6fe7e5b
Chirag Juneja <chiragjuneja6@gmail.com>
2025-12-15 17:34:16 +0530

    restructure last commit

--- fundamentals/aggregate.sql
+++ fundamentals/aggregate.sql
@@ -4,8 +4,3 @@ SELECT AVG(release_year) FROM albums;
 SELECT bands.name, COUNT(band_id) FROM bands
 JOIN albums ON albums.band_id = bands.id
 GROUP BY bands.name;
-
--- LEFT JOIN, ALIAS & GROUP BY
-SELECT bands.name AS band_name, COUNT(band_id) AS num_albums FROM bands
-LEFT JOIN albums ON albums.band_id = bands.id
-GROUP BY bands.id;
